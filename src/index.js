44fc61f22506f0277094c39e3163ef94c49a0e72
Unknown <dotasek.dev@gmail.com>
2018-12-13 16:19:01 -0800

    Revert code removed by merge from master.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var CxToJs = require('./cx_to_js.js').CxToJs;
+var CxToCyCanvas = require('./cx_to_cy_canvas.js').CxToCyCanvas;
 var CyNetworkUtils = require ('./cy_network_utils.js').CyNetworkUtils;
 
-module.exports = { CxToJs, CyNetworkUtils};
+module.exports = { CxToJs, CyNetworkUtils, CxToCyCanvas};
